c9dd82e43f40a04fa80ea0204433a069ecf18935
WarmRabbit <543138020@qq.com>
2016-05-11 23:51:13 +0800

    finish modal

--- JS/Modal.js
+++ JS/Modal.js
@@ -5,8 +5,41 @@ function alertToggle() {
     var alertModel = document.getElementsByClassName("alert-outer")[0];
     if (window.getComputedStyle(alertModel).display === "none") {
         alertModel.style.display = "block";
+        alertModel.style.height = document.body.scrollHeight+'px';
     }
     else {
         alertModel.style.display = "none";
     }
+}
+
+function showConfirmModal(){
+    var confirmModal = document.getElementsByClassName("confirm-outer")[0];
+    confirmModal.style.display = "block";
+    confirmModal.style.height = document.body.scrollHeight+'px';
+}
+
+function closeConfirmModal(){
+    var confirmModal = document.getElementsByClassName("confirm-outer")[0];
+    confirmModal.style.display = "none";
+}
+
+function confirmInfo(){
+    var confirmModal = document.getElementsByClassName("confirm-outer")[0];
+    confirmModal.style.display = "none";
+}
+
+function showPromptModal(){
+    var promptModal = document.getElementsByClassName("prompt-outer")[0];
+    promptModal.style.display = "block";
+    promptModal.style.height = document.body.scrollHeight+'px';
+}
+
+function closePromptModal(){
+    var promptModal = document.getElementsByClassName("prompt-outer")[0];
+    promptModal.style.display = "none";
+}
+
+function promptInfo(){
+    var promptModal = document.getElementsByClassName("prompt-outer")[0];
+    promptModal.style.display = "none";
 }
