61d5caa64638352cd8be566a10d5226658981cec
WarmRabbit <543138020@qq.com>
2016-05-13 07:54:30 +0800

    use DOM2

--- JS/Dropdown.js
+++ JS/Dropdown.js
@@ -2,7 +2,6 @@ var btn = document.getElementsByClassName("btn")[0];
 var dropdown = document.getElementsByClassName("basic-dropdown")[0];
 var list = dropdown.children[1];
 
-
 function showList() {
     list.style.display = "inline-block";
 }
@@ -14,8 +13,7 @@ function hideList() {
 //function showBG(event) {
 //    event.style.backgroundColor = "#000";
 //}
-
-btn.onmouseover = showList;
-btn.onmouseout = hideList;
-list.onmouseover = showList;
-list.onmouseout = hideList;
+btn.addEventListener('mouseover',showList,false);
+btn.addEventListener('mouseout',hideList,false);
+list.addEventListener('mouseover',showList,false);
+list.addEventListener('mouseout',hideList,false);
--- JS/Modal.js
+++ JS/Modal.js
@@ -1,8 +1,21 @@
 /**
  * Created by Administrator on 2016-5-8.
  */
+var alertModel = document.getElementsByClassName("alert-outer")[0];
+var confirmModal = document.getElementsByClassName("confirm-outer")[0];
+var promptModal = document.getElementsByClassName("prompt-outer")[0];
+var alertBtn1 = document.getElementsByClassName("alert-btn")[0];
+var alertBtn2 = document.getElementsByClassName("alert-btn")[1];
+var showConfirmBtn = document.getElementsByClassName("show-confirm-btn")[0];
+var closeConfirmBtn1 = document.getElementsByClassName("close-confirm-btn")[0];
+var closeConfirmBtn2 = document.getElementsByClassName("close-confirm-btn")[1];
+var confirmBtn = document.getElementsByClassName("confirm-btn")[0];
+var showPromptBtn = document.getElementsByClassName("show-prompt-btn")[0];
+var closePromptBtn1 = document.getElementsByClassName("close-prompt-btn")[0];
+var closePromptBtn2 = document.getElementsByClassName("close-prompt-btn")[1];
+var promptBtn = document.getElementsByClassName("prompt-btn")[0];
+
 function alertToggle() {
-    var alertModel = document.getElementsByClassName("alert-outer")[0];
     if (window.getComputedStyle(alertModel).display === "none") {
         alertModel.style.display = "block";
         alertModel.style.height = document.body.scrollHeight + 'px';
@@ -13,33 +26,38 @@ function alertToggle() {
 }
 
 function showConfirmModal() {
-    var confirmModal = document.getElementsByClassName("confirm-outer")[0];
     confirmModal.style.display = "block";
     confirmModal.style.height = document.body.scrollHeight + 'px';
 }
 
 function closeConfirmModal() {
-    var confirmModal = document.getElementsByClassName("confirm-outer")[0];
     confirmModal.style.display = "none";
 }
 
 function confirmInfo() {
-    var confirmModal = document.getElementsByClassName("confirm-outer")[0];
     confirmModal.style.display = "none";
 }
 
 function showPromptModal() {
-    var promptModal = document.getElementsByClassName("prompt-outer")[0];
     promptModal.style.display = "block";
     promptModal.style.height = document.body.scrollHeight + 'px';
 }
 
 function closePromptModal() {
-    var promptModal = document.getElementsByClassName("prompt-outer")[0];
     promptModal.style.display = "none";
 }
 
 function promptInfo() {
-    var promptModal = document.getElementsByClassName("prompt-outer")[0];
     promptModal.style.display = "none";
 }
+
+alertBtn1.addEventListener('click', alertToggle, false);
+alertBtn2.addEventListener('click', alertToggle, false);
+showConfirmBtn.addEventListener('click', showConfirmModal, false);
+closeConfirmBtn1.addEventListener('click', closeConfirmModal, false);
+closeConfirmBtn2.addEventListener('click', closeConfirmModal, false);
+confirmBtn.addEventListener('click', confirmInfo, false);
+showPromptBtn.addEventListener('click', showPromptModal, false);
+closePromptBtn1.addEventListener('click', closePromptModal, false);
+closePromptBtn2.addEventListener('click', closePromptModal, false);
+promptBtn.addEventListener('click', promptInfo, false);
