4625bb78a8ed8e48f68b75ecee0bfddcabe5a0db
WarmRabbit <543138020@qq.com>
2016-05-12 23:59:46 +0800

    use DOM0

--- JS/Dropdown.js
+++ JS/Dropdown.js
@@ -1,15 +1,21 @@
+var btn = document.getElementsByClassName("btn")[0];
+var dropdown = document.getElementsByClassName("basic-dropdown")[0];
+var list = dropdown.children[1];
+
+
 function showList() {
-    var dropdown = document.getElementsByClassName("basic-dropdown")[0];
-    var list = dropdown.children[1];
     list.style.display = "inline-block";
 }
 
 function hideList() {
-    var dropdown = document.getElementsByClassName("basic-dropdown")[0];
-    var list = dropdown.children[1];
     list.style.display = "none";
 }
 //to be continued
 //function showBG(event) {
 //    event.style.backgroundColor = "#000";
 //}
+
+btn.onmouseover = showList;
+btn.onmouseout = hideList;
+list.onmouseover = showList;
+list.onmouseout = hideList;
