f33e2f0e2529fc7de36a91fc60b546f5b92062c3
WarmRabbit <543138020@qq.com>
2016-05-13 20:09:46 +0800

    use event delegation

--- JS/Dropdown.js
+++ JS/Dropdown.js
@@ -2,17 +2,17 @@ var btn = document.getElementsByClassName("btn")[0];
 var dropdown = document.getElementsByClassName("basic-dropdown")[0];
 var list = dropdown.children[1];
 
-function showList() {
+//这里算事件委托嘛？
+function showList(event) {
     list.style.display = "inline-block";
+    event.target.style.backgroundColor = "#f2f2f2";
 }
 
 function hideList() {
     list.style.display = "none";
+    event.target.style.backgroundColor = "#fff";
 }
-//to be continued
-//function showBG(event) {
-//    event.style.backgroundColor = "#000";
-//}
+
 btn.addEventListener('mouseover',showList,false);
 btn.addEventListener('mouseout',hideList,false);
 list.addEventListener('mouseover',showList,false);
