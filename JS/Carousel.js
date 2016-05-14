9bfb116d4dd2ecf205fc14e8208cc713de8cf561
WarmRabbit <543138020@qq.com>
2016-05-14 23:00:04 +0800

    add auto Carousel

--- JS/Carousel.js
+++ JS/Carousel.js
@@ -19,7 +19,7 @@ function addLoadEvent(func) {
         }
     }
 }
-//点击轮播
+//点击切换
 function changeCarousel(event) {
     for (var i = 0; i < ctrlItems.length; i++) {
         (function (i) {
@@ -32,10 +32,45 @@ function changeCarousel(event) {
                 var moveDistance = -900 * i;
                 carouselMain.style.left = moveDistance + "px";
                 ctrlItems[i].children[0].style.backgroundColor = '#ccc';
-            }, false)
+                clearTimeout(timer);
+            }, false);
+            ctrlItems[i].addEventListener('mouseout', function () {
+                clearTimeout(timer);
+                //为什么这里穿进去的参数似乎没有起作用呢？ TT
+                timer = setTimeout('autoCarousel(++i)', 2500);
+            },false);
         })(i);
     }
+
 }
 
 addLoadEvent(changeCarousel);
 
+//自动轮播
+var i = 1;
+var moveDistance = 0;
+function autoCarousel() {
+    if (arguments[0] || arguments[0] === 0){
+        i = arguments[0];
+    }
+    for (var j = 0; j < ctrlItems.length; j++) {
+        carousels[j].style.zIndex = 0;
+        ctrlItems[j].children[0].style.backgroundColor = '#fff';
+    }
+    if (i == 0 || i >= 3){
+        i = 0;
+        moveDistance = 0;
+        carouselMain.style.left = moveDistance + "px";
+        ctrlItems[i].children[0].style.backgroundColor = '#ccc';
+        i++;
+    }
+    else {
+        moveDistance =  -900 * i;
+        carouselMain.style.left = moveDistance + "px";
+        ctrlItems[i].children[0].style.backgroundColor = '#ccc';
+        i++;
+    }
+    timer = setTimeout('autoCarousel(i)', 2500);
+}
+
+var timer = setTimeout('autoCarousel()', 2500);
