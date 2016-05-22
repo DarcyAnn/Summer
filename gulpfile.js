e1d134e4176e7075c676943ba137e9b924e68f2c
WarmRabbit <543138020@qq.com>
2016-05-22 17:59:17 +0800

    monitor multiple scss files

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,13 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     plumber = require('gulp-plumber'),
     browserSync = require('browser-sync'),
+    reload = browserSync.reload,
     concat = require('gulp-concat'),
     clean = require('gulp-clean'),
     uglify = require('gulp-uglify'),
     cleanCSS = require('gulp-clean-css');
 
-//dev css
-gulp.task('stylesDev', function () {
-    return gulp.src('main.scss')
-        .pipe(plumber())
-        .pipe(sass({
-            outputStyle: 'companded'
-        }).on('error', sass.logError))
-        .pipe(gulp.dest('./'));
-});
+
 
 //dev demo.css
 gulp.task('demoStylesDev', function () {
@@ -40,45 +33,64 @@ gulp.task('browser-sync', function () {
     });
 });
 
+gulp.task('concatCSS', ['clean'], function () {
+    return gulp.src(['CSS/{common,Buttons,Forms,Girds,Tables}.scss','JS/*.scss','!CSS/demo.scss'])
+        .pipe(concat('main.scss'))
+        .pipe(gulp.dest('./'));
+});
+
+//dev css
+gulp.task('stylesDev',['concatCSS'], function () {
+    return gulp.src('main.scss')
+        .pipe(plumber())
+        .pipe(sass({
+            outputStyle: 'companded'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('concatJS', ['clean'], function () {
+    return gulp.src('JS/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('JS'));
+});
+
+//dev
+gulp.task('doStyleDev', function () {
+    /* watch .sass|.scss files */
+    gulp.watch('{CSS,JS}/*.scss', ['stylesDev','demoStylesDev']);
+});
+gulp.task('dev', ['doStyleDev', 'browser-sync'], function () {
+    gulp.watch(['index.html', '*.css', 'JS/*.js']).on('change', reload);
+});
+
 //release
 gulp.task('clean', function () {
     return gulp.src('dist/', {read: false})
         .pipe(clean());
 });
 
-gulp.task('concat', ['clean'], function () {
-    return gulp.src('JS/*.js')
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('JS'));
-});
-
-gulp.task('copy', ['clean', 'concat'], function () {
+//release
+gulp.task('copy', ['clean', 'concatJS'], function () {
     return gulp.src(['main.css', 'JS/main.js'])
         .pipe(gulp.dest('dist/'));
 });
 
+//release
 gulp.task('minifyJs', ['copy'], function () {
     return gulp.src('dist/main.js')
         .pipe(uglify())
         .pipe(gulp.dest('dist'));
 });
 
+//release
 gulp.task('minifyCss', ['copy'], function () {
     return gulp.src('dist/*.css')
         .pipe(cleanCSS())
         .pipe(gulp.dest('dist'));
 });
 
-//dev
-gulp.task('doStyleDev', function () {
-    /* watch .sass|.scss files */
-    gulp.watch('*.+(sass|scss)', ['stylesDev','demoStylesDev']);
-});
-gulp.task('dev', ['doStyleDev', 'browser-sync'], function () {
-    gulp.watch(['index.html', '*.css', 'JS/*.js']).on('change', reload);
-});
-
-
+//release
 gulp.task('release', ['minifyJs', 'minifyCss'], function () {
 
 });
